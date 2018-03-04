0db595eaf23060fe9a960b0611ac578045e3c559
David Kim <davidkim310@gmail.com>
2018-03-04 01:48:23 -0800

    complete albums app

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album
-    const { thumbnailStyle, headerContentStyle, headerTextStyle, thumbnailContainerStyle } = styles
+    const { title, artist, thumbnail_image, image, url } = album
+    const { thumbnailStyle, headerContentStyle, headerTextStyle, thumbnailContainerStyle, imageStyle } = styles
 
     return (
         <Card>
             <CardSection>
-                <View style={ thumbnailContainerStyle }>
-                    <Image style={ thumbnailStyle }
+                <View style={thumbnailContainerStyle}>
+                    <Image style={thumbnailStyle}
                         source={{ uri: thumbnail_image }} />
                 </View>
-                <View style={ headerContentStyle }>
-                    <Text style={ headerTextStyle }>{title}</Text>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{ title }</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
@@ -40,6 +51,11 @@ const styles = {
         alignItems: 'center',
         marginLeft: 10,
         marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 
 }
